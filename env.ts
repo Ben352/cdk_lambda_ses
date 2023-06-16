4454e67f3ca3b967a78f811debc7a1d9bb120230
Ben Sauber <ben.sauber@kcl.ac.uk>
2023-06-16 17:38:19 +0200

    lambda Ses schelueder first commit

---
+++ env.ts
@@ -0,0 +1,5 @@
+export const AWS_REGION_Project = 'xx-xx-x';
+export const SES_EMAIL_TO = 'xxxxxxx@xxxxx.xxxx';
+export const SES_EMAIL_FROM = 'xxxxxx@xxxxx.xxxx';
+
+//Replace these values with your custom values
--- lib/lambda_ses-stack.ts
+++ lib/lambda_ses-stack.ts
@@ -1,16 +1,46 @@
+import {Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as events from 'aws-cdk-lib/aws-events'
+import * as targets from 'aws-cdk-lib/aws-events-targets'
 import * as cdk from 'aws-cdk-lib';
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { AWS_REGION_Project,SES_EMAIL_FROM,SES_EMAIL_TO } from '../env';
 
 export class LambdaSesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaEventRule = new events.Rule(this, 'lambdaScheduler', {
+      schedule: cdk.aws_events.Schedule.rate(cdk.Duration.minutes(2)),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaSesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    // Lambda function that sends the email
+    // upload the local env file to the lambda environment
+  const walletMonitor = new lambda.Function(this, 'lambdaEmail', {
+      runtime: lambda.Runtime.NODEJS_16_X,   
+      code: lambda.Code.fromAsset('lambda'),  
+      handler: 'lambdaEmailSender.handler',
+      environment: {
+        AWS_REGION_Project: AWS_REGION_Project,
+        SES_EMAIL_FROM: SES_EMAIL_FROM,
+        SES_EMAIL_TO: SES_EMAIL_TO,
+      }              
+    });
+
+    // Only give the lambda the function to send an email from a specific email address
+    // and also only from a specific region
+    walletMonitor.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["ses:SendEmail"],
+        resources: [`arn:aws:ses:${AWS_REGION_Project}:${
+          cdk.Stack.of(this).account
+        }:identity/${SES_EMAIL_FROM}`],
+      })
+    );
+    // Add the scheduler to the lambda function
+    lambdaEventRule.addTarget(new targets.LambdaFunction(walletMonitor))
+    
   }
 }
